ef0facd077a6110f3f97df0ce6fe8c2cfd229051
Khrystyna Harasym <109437395+Khrystyna-Harasym@users.noreply.github.com>
2022-12-21 21:08:18 -0500

    completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+
+const stopBtn = document.querySelector('button[data-stop]');
+
+let colorBg = null;
+
+startBtn.addEventListener('click', () => {
+
+    colorBg = setInterval(() => {
+      const randomBgc = getRandomHexColor();
+        document.body.style.backgroundColor = randomBgc;
+       
+    }, 1000);
+     startBtn.setAttribute('disabled', '');
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(colorBg);
+    stopBtn.removeAttribute('disabled', '');
+});
